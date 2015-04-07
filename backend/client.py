55fca3cd7ffbc6358edd2f1d6c097f4247ffcdfd
mockingbird2 <fabian.paul1@gmx.net>
2015-04-07 15:16:34 +0200

    json validation

--- backend/client.py
+++ backend/client.py
@@ -1,5 +1,22 @@
 # Client thread started for each player
+import urllib2
+import json
+
+def main(ll):
+    response = urllib2.urlopen('http://m.mobile.de/svc/s/?ll=' + ll)
+    data = json.load(response)['items']
+    gameCars = validation(data)
+    print gameCars
+
+def validation(dict):
+    resList = []
+    while len(resList) <= 20:
+        for elem in dict:
+            if elem['details'] > 6:
+                resList.append(elem['details'])
+    return resList
+
+if __name__ == '__main__':
+    main()
 
 
-def main():
-	
