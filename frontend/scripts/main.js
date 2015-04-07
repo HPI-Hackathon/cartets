de9598c64f49c9e0c0b56daab95eb46f6073a79e
René Springer <post@rene-springer.de>
2015-04-07 16:03:11 +0200

    geolocation error handling + display form to insert username after location has been fetched

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -3,15 +3,15 @@
 
 var connection = new WebSocket('ws://localhost:8080', []);
 
-// connection.onopen = function () {
-//     connection.send('Ping');
-//     console.info('connected to server');
-// };
+connection.onopen = function () {
+    connection.send('Ping');
+    console.info('connected to server');
+};
 
-// // Log errors
-// connection.onerror = function (error) {
-//     console.log('WebSocket Error ' + error);
-// };
+// Log errors
+connection.onerror = function (error) {
+    console.log('WebSocket Error ' + error);
+};
 
 // // Log messages from the server
 // connection.onmessage = function (e) {
@@ -20,8 +20,9 @@ var connection = new WebSocket('ws://localhost:8080', []);
 //     console.log('Server: ' + e.data);
 // };
 
-function UI () {
+function UI (socket) {
     var self = this;
+    self.socket = socket;
     self.player = {
         name: ''
     };
@@ -33,13 +34,40 @@ function UI () {
             lat: position.coords.latitude,
             lng: position.coords.longitude
         };
+        $('#locationMessage').hide();
+        $('#usernameForm').show();
     };
 
     self.positionError = function (err) {
+        var message = '';
+        if (err.code === 1) {
+            message = 'Du musst deinen Standort mitteilen, um spielen zu können.';
+        } else if (err.code === 2) {
+            message = 'Dein Standort konnte nicht ermittelt werden.';
+        }
+        $('#locationMessage')
+            .removeClass('panel-default')
+            .addClass('panel-danger');
+        $('#locationMessage .panel-body').html(message);
         console.log(err.code);
     };
+
+    $('#usernameForm').submit(function (e) {
+        var data = {
+            action: 'start',
+            username: '',
+            lat: self.position.latitude,
+            lng: self.position.longitude
+        };
+        e.preventDefault();
+        if ($.trim($('#usernameForm #username').val()) === '') {
+            alert('kein Username eingegeben');
+        } else {
+            self.socket.send(JSON.stringify(data));
+        }
+    });
 
     navigator.geolocation.getCurrentPosition(self.setPosition, self.positionError);
 }
 
-var userInterface = new UI();
+var userInterface = new UI(connection);
