ce4469c9e0245e9e9b1d5b7b089b940dd6f49c99
mockingbird2 <fabian.paul1@gmx.net>
2015-04-07 17:25:21 +0200

    changed pagesize = 100

--- backend/parser.py
+++ backend/parser.py
@@ -1,29 +1,31 @@
 # Client thread started for each player
 import urllib2
 import json
+import random
 
 def main():
     #response = urllib2.urlopen('http://m.mobile.de/svc/s/?ll=' + ll)
-    response = urllib2.urlopen('http://m.mobile.de/svc/s/?ll=52.516,13.376&s=Car')
+    response = urllib2.urlopen('http://m.mobile.de/svc/s/?ll=52.516,13.376&s=Car&psz=100')
     data = json.load(response)['items']
     gameCars = validation(data)
     print gameCars
 
-def validation(dict):
+def validation(list):
     resList = []
     while len(resList) <= 20:
-        for elem in dict:
-            if 'images' in elem and 'pw' in elem['attr']:
-                if 'csmpt' in elem['attr']:
-                    image = elem['images'][0].get('uri')+'_8.jpg'
-                    title = elem['title']
-                    price = elem['price']['grs']['amount']
-                    power = elem['attr']['pw']
-                    dist = elem['attr']['ml']
-                    fr = elem['attr']['fr']
-                    consumption = elem['attr']['csmpt']
-                    current = {title: (image, price, fr, dist, power, consumption)}
-                    resList.append(current)
+        #for elem in dict:
+        elem = random.choice(list)
+        if 'images' in elem and 'pw' in elem['attr']:
+            if 'csmpt' in elem['attr']:
+                image = elem['images'][0].get('uri')+'_8.jpg'
+                title = elem['title']
+                price = elem['price']['grs']['amount']
+                power = elem['attr']['pw']
+                dist = elem['attr']['ml']
+                fr = elem['attr']['fr']
+                consumption = elem['attr']['csmpt']
+                current = {title: (image, price, fr, dist, power, consumption)}
+                resList.append(current)
 
     return resList
 
