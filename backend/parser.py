8598d5d2c8a93359eccf538bb82016b7759860ae
mockingbird2 <fabian.paul1@gmx.net>
2015-04-07 17:39:43 +0200

    sort by range

--- backend/parser.py
+++ backend/parser.py
@@ -5,7 +5,7 @@ import random
 
 def main():
     #response = urllib2.urlopen('http://m.mobile.de/svc/s/?ll=' + ll)
-    response = urllib2.urlopen('http://m.mobile.de/svc/s/?ll=52.516,13.376&s=Car&psz=100')
+    response = urllib2.urlopen('http://m.mobile.de/svc/s/?ll=52.516,13.376&s=Car&psz=100&sb=doc')
     data = json.load(response)['items']
     gameCars = validation(data)
     print gameCars
@@ -15,7 +15,8 @@ def validation(list):
     while len(resList) <= 20:
         #for elem in dict:
         elem = random.choice(list)
-        if 'images' in elem and 'pw' in elem['attr']:
+
+        if 'images' in elem and 'pw' in elem['attr'] and 'ml' in elem['attr']:
             if 'csmpt' in elem['attr']:
                 image = elem['images'][0].get('uri')+'_8.jpg'
                 title = elem['title']
