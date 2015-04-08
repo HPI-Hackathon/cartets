08ebd37ad0fa2ecb1db51be8b2a7d11fa8e9edb2
Lawrence Benson <lawrence.benson@student.hpi.uni-potsdam.de>
2015-04-08 06:54:05 +0200

    Improve card_parser

--- backend/card_parser.py
+++ backend/card_parser.py
@@ -11,16 +11,23 @@ def main(long, lat):
     url = 'http://m.mobile.de/svc/s/?ll=' + str(long) + ',' + str(lat) + '&s=Car&psz=100&sb=doc'
     response = urllib2.urlopen(url)
     data = json.load(response)['items']
-    gameCars = validation(data)
-    return gameCars
+    return generate_list(data)
 
 
-def validation(list):
+def generate_list(list):
     res_list = []
     while len(res_list) <= 10:
         elem = random.choice(list)
+        item = validate_entry(elem)
+        if item:
+            res_list.append(item)
+    return res_list
 
-        if 'images' in elem and 'pw' in elem['attr'] and 'ml' in elem['attr'] and 'csmpt' in elem['attr']:
+
+def validate_entry(elem):
+    attr_keys = ['pw', 'ml', 'csmpt']
+    if 'images' in elem and all(key in elem['attr'] for key in attr_keys):
+        try:
             image = elem['images'][0].get('uri')+'_8.jpg'
             title = elem['title']
             price = elem['price']['grs']['amount']
@@ -29,23 +36,20 @@ def validation(list):
             first = '.'.join(reversed(elem['attr']['fr'].split('/')))
             location = '{:f},{:f}'.format(elem['contact']['latLong']['lat'], elem['contact']['latLong']['lon'])
             url = elem['url']
-            try:
-                consumption = re.search('\d+\,\d+', elem['attr']['csmpt']).group(0).replace(',','.')
-            except:
-                continue
-            current = {'title': title,
-                       'image': image,
-                       'price': price,
-                       'registration': first,
-                       'mileage': dist,
-                       'power': power,
-                       'consumption': consumption,
-                       'location': location,
-                       'url': url}
-            final = json.dumps(current)
-            res_list.append(final)
+            consumption = re.search('\d+\,\d+', elem['attr']['csmpt']).group(0).replace(',','.')
 
-    return res_list
+        except:
+            return
+
+        return json.dumps({'title': title,
+                           'image': image,
+                           'price': price,
+                           'registration': first,
+                           'mileage': dist,
+                           'power': power,
+                           'consumption': consumption,
+                           'location': location,
+                           'url': url})
 
 
 if __name__ == '__main__':
